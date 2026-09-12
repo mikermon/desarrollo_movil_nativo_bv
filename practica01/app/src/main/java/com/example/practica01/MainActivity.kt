e2785aabae51724e6673af6a24b70ce8562f4ccc
mikermon <roarsoyundinosaurioroar@gmail.com>
2026-09-12 16:38:33 -0700

    paso 7

--- practica01/app/src/main/java/com/example/practica01/MainActivity.kt
+++ practica01/app/src/main/java/com/example/practica01/MainActivity.kt
@@ -18,30 +18,9 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Practica01Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+
+
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Practica01Theme {
-        Greeting("Android")
-    }
-}
