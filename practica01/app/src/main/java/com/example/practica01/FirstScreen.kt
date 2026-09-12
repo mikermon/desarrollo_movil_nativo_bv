04a612bf336c4b061cbc21d0578017c503ebe953
mikermon <roarsoyundinosaurioroar@gmail.com>
2026-09-12 16:49:07 -0700

    paso 8.5
    faltaban importar librerías

--- practica01/app/src/main/java/com/example/practica01/FirstScreen.kt
+++ practica01/app/src/main/java/com/example/practica01/FirstScreen.kt
@@ -1,4 +1,12 @@
 package com.example.practica01
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
 @Composable
 fun FirstScreen() {
     var textoIngresado by remember { mutableStateOf("") }
