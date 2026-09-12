e0d0b93636e9fdede319c9436619cb6b7c469b1f
mikermon <roarsoyundinosaurioroar@gmail.com>
2026-09-12 16:45:21 -0700

    paso 8
    tiene cosas que cambiar ya que me marca error en varias lineas

--- practica01/app/src/main/java/com/example/practica01/FirstScreen.kt
+++ practica01/app/src/main/java/com/example/practica01/FirstScreen.kt
@@ -1,2 +1,32 @@
 package com.example.practica01
+@Composable
+fun FirstScreen() {
+    var textoIngresado by remember { mutableStateOf("") }
+    Scaffold(
+        containerColor = Color(0xFFE3F2FD)
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            OutlinedTextField(
+                value = textoIngresado,
+                onValueChange = { textoIngresado = it },
+                label = { Text("Escribe tu texto") },
+                modifier = Modifier.fillMaxWidth()
+            )
 
+            Spacer(modifier = Modifier.height(20.dp))
+
+            Button(
+                onClick = { }
+            ) {
+                Text("Ir a SecondScreen")
+            }
+        }
+    }
+}
