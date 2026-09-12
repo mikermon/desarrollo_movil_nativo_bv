dc4aaf392d0c2829f99bbd6e6714bab54148b584
mikermon <roarsoyundinosaurioroar@gmail.com>
2026-09-12 16:57:15 -0700

    paso 10

--- practica01/app/src/main/java/com/example/practica01/SecondScreen.kt
+++ practica01/app/src/main/java/com/example/practica01/SecondScreen.kt
@@ -1,2 +1,36 @@
 package com.example.practica01
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+@Composable
+fun SecondScreen(mensaje: String) {
+    Scaffold(
+        containerColor = Color(0xFFF3E5F5)
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "Texto recibido:",
+                fontSize = 18.sp
+            )
 
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Text(
+                text = mensaje,
+                fontSize = 24.sp
+            )
+        }
+    }
+}
