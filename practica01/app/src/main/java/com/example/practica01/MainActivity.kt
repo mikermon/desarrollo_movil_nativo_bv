ac781f7cbc5f906c184b332bd1dae2471b6d80bb
mikermon <roarsoyundinosaurioroar@gmail.com>
2026-09-12 18:26:44 -0700

    correccion del paso 10 u 11

--- practica01/app/src/main/java/com/example/practica01/MainActivity.kt
+++ practica01/app/src/main/java/com/example/practica01/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Practica01Theme() {
-                FirstScreen()
+                AppNavigation()
             }
 
         }
